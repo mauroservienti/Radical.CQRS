3ef620f0dc96cc6aa90bb81d4c509b699c37233f
mauroservienti <mauro@topics.it>
2016-03-12 13:31:28 +0100

    Initial support for #12

---
+++ src/Radical.CQRS.Messages/IAggregateCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radical.CQRS.Messages
+{
+    public interface IAggregateCommand
+    {
+        Guid Id { get; }
+        int Version { get; }
+    }
+}
